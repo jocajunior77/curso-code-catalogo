0012ce6fda9a0412743f7d5bcbd387f021a7bee6
(Joca) <jocajunior77@gmail.com>
2020-03-26 15:50:07 -0300

    uuid and softdelete

--- app/Models/Genre.php
+++ app/Models/Genre.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Ramsey\Uuid\Uuid;
 
 class Genre extends Model
 {
@@ -15,4 +16,13 @@ class Genre extends Model
     ];
 
     protected $dates = ['deleted_at'];
+
+    public static function boot()
+    {
+        parent::boot();
+        static::creating(function($obj) {
+            $obj->id = Uuid::uuid4();
+        });
+
+    }
 }
